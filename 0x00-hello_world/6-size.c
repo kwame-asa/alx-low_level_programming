9488da9275ff81b5f81622a3b87e67cf9b11ee47
Prince Asiedu <princeasa99@gmail.com>
2023-06-18 12:58:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
  * Return: 0 (Success)
  */
-int main(void
-		)
-	{
+int main(void)
+{
 	char a;
 	int b;
 	long int c;
